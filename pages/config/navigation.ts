5cda81f1c7bdf4b712bbc7787ddf24e89f11c943
Karan <karan@karans-air.home>
2024-01-08 14:16:14 +0100

    twitter-layout initial

---
+++ pages/config/navigation.ts
@@ -0,0 +1,12 @@
+import {ReactNode} from "react";
+import { BiHomeCircle } from "react-icons/bi";
+
+interface TwitterSideBarNavigation {
+	title: string;
+	icon: ReactNode;
+}
+
+
+const sideBarMenus: TwitterSideBarNavigation[]=[
+  
+]
